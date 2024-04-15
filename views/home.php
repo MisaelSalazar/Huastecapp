4f3e42f5b123d35ef5ae75517922d9526ff28010
MisaelSalazar <misaelsalazarof@protonmail.com>
2024-04-15 13:25:57 -0600

    Foldres

---
+++ views/home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../public/fontawesome-free-6.5.1-web/all.css">
+
+    <link rel="stylesheet" href="../css/bottomNavBar.css">
+    <link rel="stylesheet" href="../css/all.css">
+</head>
+
+<body>
+    <h1>hola</h1>
+
+    <header>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        <nav class="bottom-navbar">
+
+            <a href="views/juegos.php" class="nav-item">
+                <i class="fa-solid fa-gamepad"></i><br>
+                <span class="text">Juegos</span>
+            </a>
+
+            <a href="views/contactos.php" class="nav-item">
+                <i class="fa-solid fa-address-book"></i><br>
+                <span class="text">Contactos</span>
+            </a>
+
+            <a href="views/mapas.php" class="nav-item">
+                <i class="fa-solid fa-earth-americas"></i><br>
+                <span class="text">Mapas</span>
+            </a>
+
+        </nav>
+    </footer>
+
+
+    <script src="../public/fontawesome-free-6.5.1-web/all.js"></script>
+</body>
+
+</html>
